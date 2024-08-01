69dc4436fd85e83dec5bfb8d51e7d4e42cc83d2b
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-01 23:09:16 +0900

    heroku

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -87,5 +87,4 @@ Rails.application.configure do
   # ]
   # # Skip DNS rebinding protection for the default health check endpoint.
   # # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  #config.hosts << "interview2-backend-20779016b09c.herokuapp.com"
 end
