26513cb28bac725e477348dea64a3344e9463efe
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-01 22:39:50 +0900

    Allow Heroku host

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -82,9 +82,10 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
-  #   "https://interview2-backend-20779016b09c.herokuapp.com/",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  #    "https://interview2-backend-20779016b09c.herokuapp.com/",     # Allow requests from example.com
+  # #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # # Skip DNS rebinding protection for the default health check endpoint.
+  # # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "interview2-backend-20779016b09c.herokuapp.com"
 end
