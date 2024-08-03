4fa467cd3ad671f47c221cc574187e63b7a60bb6
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-03 10:57:12 +0900

    環境変数

--- frontend/src/config/settings.js
+++ frontend/src/config/settings.js
@@ -1,5 +1,5 @@
 
 /*環境変数 REACT_APP_API_URL から API のベース URL を取得しつつデフォルト値も設定しておく*/
 export const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://interview-frontend-05a5d7363eca.herokuapp.com";
+process.env.REACT_APP_API_URL ||
+"https://interview-frontend-05a5d7363eca.herokuapp.com";
