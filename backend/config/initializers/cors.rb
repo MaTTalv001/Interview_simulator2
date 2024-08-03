55fd201ef7434ba4e45b4826c271f95c74167fcd
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-03 11:10:23 +0900

    環境変数

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8000", "example.com"
+    origins "localhost:8000", "https://interview-frontend-05a5d7363eca.herokuapp.com/"
 
     resource "*",
       headers: :any,
