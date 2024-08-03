b438c6efb001c637909ee9b1aca4a32624e26938
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-03 10:44:19 +0900

    ビルド時の環境変数読み込み改善

--- frontend/src/config/settings.js
+++ frontend/src/config/settings.js
@@ -1,5 +1,4 @@
 
 /*環境変数 REACT_APP_API_URL から API のベース URL を取得しつつデフォルト値も設定しておく　*/
 export const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "http://localhost:3000";
+  process.env.REACT_APP_API_URL
