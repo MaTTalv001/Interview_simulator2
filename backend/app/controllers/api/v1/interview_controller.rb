4fd030c4a625e2edc8b561a391f52b6435a352c3
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-20 22:19:33 +0900

    口パク動画再生

--- backend/app/controllers/api/v1/interview_controller.rb
+++ backend/app/controllers/api/v1/interview_controller.rb
@@ -4,15 +4,18 @@ class Api::V1::InterviewController < ApplicationController
       chatgpt_service = ChatgptService.new
       text_to_speech_service = TextToSpeechService.new
   
-      system_prompt = "あなたは、エンジニア面接担当者です。初回は挨拶をしましょう。その後、ユーザーのreadmeファイルを見て何か質問を考えてください。質問は1つずつとします。途中で話題を変えても構いません。readmeファイルがない場合や内容が乏しい場合は、エンジニア採用の面接にありそうな質問をしてください。"
+      system_prompt = "あなたは、エンジニア面接担当者です。初回は挨拶をしましょう。ユーザーからは「面接の想定質問」または「リポジトリのreadmeファイル」が入力されます。それを見て、面接担当者として質問を考えてください。質問は1つずつとします。途中で話題を変えても構いません。ユーザーの入力がない場合や内容が乏しい場合は、エンジニア採用の面接にありそうな質問をしてください。"
       
       messages = [
         { role: 'system', content: system_prompt },
-        { role: 'user', content: "README:\n#{readme}" }
+        { role: 'user', content: "入力:\n#{readme}" }
       ]
   
       chatgpt_response = chatgpt_service.conversation_chat(messages)
       audio_data = text_to_speech_service.generate_speech(chatgpt_response)
+
+      # アシスタントの応答をmessagesに追加
+      messages << { role: 'assistant', content: chatgpt_response }
   
       render json: { text: chatgpt_response, audio: audio_data, messages: messages }
     end
