27330df4fd01fca9b5a93357dd8c0c081415db3f
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-24 21:14:08 +0900

    iphone対応

--- backend/app/services/speech_to_text_service.rb
+++ backend/app/services/speech_to_text_service.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'streamio-ffmpeg'
 
 class SpeechToTextService
   include HTTParty
@@ -14,14 +15,18 @@ class SpeechToTextService
   end
 
   def transcribe(audio_file)
+    file_to_send = need_conversion?(audio_file) ? convert_to_mp3(audio_file) : audio_file
+    
     body = {
       multipart: true,
       model: 'whisper-1',
-      file: audio_file,
+      file: File.new(file_to_send.path, 'rb'),
       language: "ja"
     }
 
     response = self.class.post('/audio/transcriptions', body: body, headers: @options[:headers])
+
+    File.delete(file_to_send.path) if file_to_send.is_a?(Tempfile) && File.exist?(file_to_send.path)
 
     if response.success?
       response.parsed_response['text']
@@ -31,6 +36,18 @@ class SpeechToTextService
   end
 
   private
+
+  def need_conversion?(file)
+    movie = FFMPEG::Movie.new(file.path)
+    !['mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm'].include?(movie.audio_codec)
+  end
+
+  def convert_to_mp3(input_file)
+    temp_file = Tempfile.new(['converted', '.mp3'])
+    movie = FFMPEG::Movie.new(input_file.path)
+    movie.transcode(temp_file.path, audio_codec: 'libmp3lame')
+    temp_file
+  end
 
   def handle_error_response(response)
     error_message = response.parsed_response['error']['message'] rescue '不明なエラーが発生しました'
